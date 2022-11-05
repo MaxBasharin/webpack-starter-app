6d4fd5efd4e37f7b35859f671ac7772db3268652
MaxBasharin <112333199+MaxBasharin@users.noreply.github.com>
2022-11-05 22:48:33 +0300

    плагины для работы с css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import AppService from './modules/app.service'
 import { config } from './modules/config'
 import './modules/header.component'
+import './css/index.css'
 console.log('config key', config.key)
 const service = new AppService('export class')
 service.log()
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,33 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
-
-module.exports = { 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    }, optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
     },
+
     plugins: [
         new HTMLPlugin({
             filename: 'index.html',
             template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/, // Обрабатывать файлы расширения css
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    }
 }
